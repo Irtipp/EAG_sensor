f7cfb4e40c85a82fe74ab5c21dcdde897ef24282
Isaac Tipping <irtipping@icloud.com>
2025-10-24 20:07:10 -0400

    Update comments and labels in final.m

--- final.m
+++ final.m
@@ -1,28 +1,34 @@
-% DHT11TemperatureHumidity.mlx
-% Author: Isaac Tipping
-% Date: Oct 7, 2025
+% Temperature and Noise Level
+% Author: Isaac Tipping, John Aufderhar, Luke Taylor
+% Date: Oct 24, 2025
 clc
 clear
 close all
-% Connect to Arduino (adjust COM port if needed)
+% Connect to Arduino
 a = arduino("COM5", "Uno", 'Libraries', 'Adafruit/DHTxx');
+
 % Create DHT sensor object on digital pin D3
 dhtSensor = addon(a, 'Adafruit/DHTxx', 'D3', 'DHT11');
+
 % Initialize data storage
 Time = [];
 Temperature = [];
 Humidity = [];
 disp("Starting measurements...");
 tic();
+
 % Parameters
 Fs = 44100;       % Sampling rate
 nBits = 16;       % Bits per sample
 nChannels = 1;    % Mono
 duration = 10;     % Recording duration in seconds
+
 % Create audiorecorder object
 recObj = audiorecorder(Fs, nBits, nChannels);
 disp('Start speaking.');
 record(recObj);
+
+%Temperature readings
 for i = 1:10
     % Read data from the sensor
     [tempC] = readTemperature(dhtSensor);
@@ -33,8 +39,10 @@ for i = 1:10
   
     pause(1); % wait 1 second between readings
 end
+
 stop(recObj);
 disp('End of recording.');
+
 % Get the audio data as a numeric array
 audioData = getaudiodata(recObj);
 % Time vector
@@ -43,8 +51,8 @@ t = (0:length(audioData)-1)/Fs;
 figure;
 subplot(2,1,1);
 plot(t, audioData);
-xlabel('Time (s)');
-ylabel('Amplitude');
+xlabel('Time (t) [s]');
+ylabel('Amplitude (A) [idk]');
 title('Captured Audio Signal');
 grid on;
 % ---- RMS calculation ----
@@ -54,11 +62,12 @@ disp(['RMS Amplitude: ', num2str(rms_amplitude)]);
 epsilon = 1e-12; % small number to avoid log(0)
 audio_dB = 20*log10(abs(audioData) + epsilon);
 % Optional: Smooth the dB signal
-windowSize = 50000;  %Change to larger values to get a smoother graph, change to smaller numbers to make it more spikey
+windowSize = 50000; 
 audio_dB_smooth = movmean(audio_dB, windowSize);
 % ---- Plot dB over time ----
 plot(t, audio_dB_smooth);
-xlabel('Time (s)');
+xlabel('Time (t) [s]');
+ylable('Sound (s) [dB]')
 title('Smoothed Audio Signal in Decibels');
 grid on;
 % ---- Optional: Play back the recording ----
@@ -66,11 +75,12 @@ grid on;
 disp('Playing recorded audio...');
 play(recObj);
 disp("Measurements complete.");
-% Plot results
+
+% Plot temp
 subplot(2,1,2)
 plot(Time, Temperature, 'r-o');
 title('Temperature over Time');
-xlabel('Time [s]');
-ylabel('Data');
+xlabel('Time (t) [s]');
+ylabel('Temperature (T) [degC]');
 legend('Sound (s) dB', 'Temp (t) DegC]')
 grid on;
